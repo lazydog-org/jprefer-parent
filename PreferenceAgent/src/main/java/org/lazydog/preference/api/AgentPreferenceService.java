9983c9c12465fab27d25b55dc54dd0131528c8ca
rjrjr <rjrjr@cox.net>
2010-01-30 21:05:45 +0000

    make PreferenceAgent a module of PreferenceManager - rjrjr

---
+++ PreferenceAgent/src/main/java/org/lazydog/preference/api/AgentPreferenceService.java
@@ -0,0 +1,14 @@
+package org.lazydog.preference.api;
+
+import org.lazydog.preference.service.LocalPreferenceService;
+
+
+/**
+ * Agent preference service.
+ * 
+ * @author  Ron Rickard
+ */
+public class AgentPreferenceService
+        extends LocalPreferenceService
+        implements AgentPreferenceServiceMBean {
+}
