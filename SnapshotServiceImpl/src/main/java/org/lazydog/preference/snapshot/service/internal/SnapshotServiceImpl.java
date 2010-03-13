717c4929685e74eb5b26d9fd57c065e13cb6f620
rjrjr <rjrjr@cox.net>
2010-03-13 22:29:03 +0000

    minor fixes; added Snapshot service - rjrjr

---
+++ SnapshotServiceImpl/src/main/java/org/lazydog/preference/snapshot/service/internal/SnapshotServiceImpl.java
@@ -0,0 +1,13 @@
+package org.lazydog.preference.snapshot.service.internal;
+
+import org.lazydog.preference.snapshot.service.SnapshotService;
+
+
+/**
+ * Snapshot service implementation.
+ *
+ * @author  Ron Rickard
+ */
+public class SnapshotServiceImpl implements SnapshotService {
+
+}
