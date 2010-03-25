fca806f9c66950b913613d2137e551c7d5ad0c64
rjrjr <rjrjr@cox.net>
2010-03-25 00:13:09 +0000

    created custom Path validator - rjrjr

---
+++ preference-manager-utility/src/main/java/org/lazydog/preference/manager/validation/constraints/Path.java
@@ -0,0 +1,24 @@
+package org.lazydog.preference.manager.validation.constraints;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+
+/**
+ * Path constraint.
+ *
+ * @author  Ron Rickard
+ */
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PathValidator.class)
+public @interface Path {
+
+    String message() default "{org.lazydog.preference.manager.validation.constraints.Path}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ preference-manager-utility/src/main/java/org/lazydog/preference/manager/validation/constraints/PathValidator.java
@@ -0,0 +1,38 @@
+package org.lazydog.preference.manager.validation.constraints;
+
+import java.util.regex.Pattern;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+
+/**
+ * Path validator.
+ *
+ * @author  Ron Rickard
+ */
+public class PathValidator implements ConstraintValidator<Path, String> {
+
+    public static final String VALUE_REGEX = "^/[\\w]+(/[\\w]+)*$";
+
+    /**
+     * Initialize.
+     *
+     * @param  path  the path constraint.
+     */
+    @Override
+    public void initialize(Path path) {
+    }
+
+    /**
+     * Is the value valid?
+     *
+     * @param  value    the value.
+     * @param  context  the constraint validator context.
+     *
+     * @return  true if the value is valid, otherwise false.
+     */
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return Pattern.matches(VALUE_REGEX, value);
+    }
+}
